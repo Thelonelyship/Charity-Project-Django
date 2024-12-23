a0c2b55228e73b3f9b67869247b28f890f121497
Thelonelyship <allanbram@hotmail.com>
2024-12-23 23:40:24 +0000

    Updated settings and requirements

--- charity/settings.py
+++ charity/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for charity project.
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-$e9noqk4ft1@s$u1y3@y+0kcl-hzm(iw(s(4&#n5!f%tw9g=(b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
